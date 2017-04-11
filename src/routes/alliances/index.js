50f70b06b485c0497c651ecad9f0d02afce298d7
Robert Schuh <gitlab@eneticum.de>
2017-04-11 19:31:33 +0200

    small changes

--- src/routes/alliances/index.js
+++ src/routes/alliances/index.js
@@ -2,6 +2,7 @@
   import React from 'react';
   import Layout from '../../components/Layout';
   import Alliance from './Alliance';
+  import { set_loading } from '../../actions/layout';
 
   export default [{
 
@@ -9,8 +10,9 @@
 
     async action(act) {
       console.log('alli route', act);
-      const { path } = act;
-      const props = await Promise.resolve({
+      const { store, path } = act;
+      store.dispatch(set_loading(true));
+      const props = await new Promise(resolve => setTimeout(() =>resolve({
         id: 498125261,
         name: 'Test Alliance Please Ignore',
         ticker: 'TEST',
@@ -18,7 +20,8 @@
           id: 1234,
           name: 'Upvote',
         },
-      });
+      }), 5000));
+      store.dispatch(set_loading(false));
       return {
         title: 'Alliance',
         component: <Layout location={{ path }}><Alliance {...props} /></Layout>,
