46ae806caa433e4aeeb9b4d587ebe4b52529f1bf
Robert Schuh <github@eneticum.de>
2017-04-26 23:19:31 +0200

    html

--- src/components/Html.js
+++ src/components/Html.js
@@ -39,10 +39,19 @@ class Html extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="mobile-web-app-capable" content="yes" />
           <title>{title}</title>
           <meta name="description" content={description} />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+          <link rel="search" type="application/opensearchdescription+xml" href="/xml/search.xml" title="search" />
           <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+          <link rel="stylesheet" type="text/css" href="/css/bg.css" />
+          <link rel="stylesheet" type="text/css" href="/css/main.css" />
+          <script src="/js/particleground.js" />
+          <script type="text/javascript" src="/js/ccpwgl/glMatrix-0.9.5.min.js" />
+          <script type="text/javascript" src="/js/ccpwgl/ccpwgl_int.js" />
+          <script type="text/javascript" src="/js/ccpwgl/ccpwgl.js" />
+          <script type="text/javascript" src="/js/ccpwgl/TestCamera2.js" />
           {styles.map(style =>
             <style
               key={style.id}
@@ -53,6 +62,22 @@ class Html extends React.Component {
           )}
         </head>
         <body>
+          <svg width="0" height="0" viewBox="0 0 1 1">
+            <defs>
+              <clipPath id="pone" clipPathUnits="objectBoundingBox">
+                <polygon points="0 0, 1 0, 0.75 1, 0 1" />
+              </clipPath>
+              <clipPath id="ptwo" clipPathUnits="objectBoundingBox">
+                <polygon points="0.2 0, 1 0, 0.8 1, 0 1" />
+              </clipPath>
+              <clipPath id="pthree" clipPathUnits="objectBoundingBox">
+                <polygon points="0.25 0, 1 0, 1 1, 0 1" />
+              </clipPath>
+              <clipPath id="pfull" clipPathUnits="objectBoundingBox">
+                <polygon points="0 0, 1 0, 1 1, 0 1" />
+              </clipPath>
+            </defs>
+          </svg>
           <div
             id="app"
             // eslint-disable-next-line react/no-danger
