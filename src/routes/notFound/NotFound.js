1bff55f672243c5bd5ed539b2beada6a84ad05e1
Robert Schuh <github@eneticum.de>
2017-04-26 22:56:09 +0200

    adjust notfound

--- src/routes/notFound/NotFound.js
+++ src/routes/notFound/NotFound.js
@@ -19,11 +19,8 @@ class NotFound extends React.Component {
 
   render() {
     return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>{this.props.title}</h1>
-          <p>Sorry, the page you were trying to view does not exist.</p>
-        </div>
+      <div className="page">
+        <h1>404 - not found</h1>
       </div>
     );
   }
