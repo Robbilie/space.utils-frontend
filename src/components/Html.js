0fd3e7bb1f7ca5e59a514dcbbdbdc93929b1c110
Robert Schuh <github@eneticum.de>
2017-05-16 18:55:43 +0200

    move ga

--- src/components/Html.js
+++ src/components/Html.js
@@ -67,9 +67,15 @@ class Html extends React.Component {
           {analytics.googleTrackingId &&
           <script
             // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html:
-            'window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;' +
-            `ga('create','${analytics.googleTrackingId}','auto');ga('send','pageview')` }}
+            dangerouslySetInnerHTML={{ __html: `
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+              ga('create', '${analytics.googleTrackingId}', 'auto');
+              ga('send', 'pageview');
+            ` }}
           />
           }
           {analytics.googleTrackingId &&
