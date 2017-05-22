4e9705492837b1211d7ac56bba3909ef6adf9d1b
Robert Schuh <github@eneticum.de>
2017-05-23 01:30:44 +0200

    should work right?

--- src/utils/EASClient.js
+++ src/utils/EASClient.js
@@ -3,12 +3,14 @@ import Swagger from 'swagger-client';
 
 const UA = typeof process !== 'undefined' && process.env.UA ? process.env.UA : 'https://utils.space/';
 
-const EASClient = new Swagger({
+const create_client = () => new Swagger({
   url: 'https://api.utils.space/api-docs',
   requestInterceptor: (req) => {
     req.headers['X-User-Agent'] = UA;
     return req;
   },
-});
+}).catch(e => console.log('EASClient Error:', e) || create_client());
+
+const EASClient = create_client();
 
 export default EASClient;
--- src/utils/ESIClient.js
+++ src/utils/ESIClient.js
@@ -3,12 +3,14 @@ import Swagger from 'swagger-client';
 
 const UA = typeof process !== 'undefined' && process.env.UA ? process.env.UA : 'https://utils.space/';
 
-const ESIClient = new Swagger({
+const create_client = () => new Swagger({
   url: 'https://esi.tech.ccp.is/_latest/swagger.json',
   requestInterceptor: (req) => {
     req.headers['X-User-Agent'] = UA;
     return req;
   },
-});
+}).catch(e => console.log('ESIClient Error:', e) || create_client());
+
+const ESIClient = create_client();
 
 export default ESIClient;
